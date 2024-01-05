56478f8f2ba9607252ac6d73384f84da664c75aa
Gloria Ciavarrini <gciavarrini@redhat.com>
2024-01-05 14:47:22 +0100

    Regenerate schema after update
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- plugins/orchestrator-common/api/models/schema.d.ts
+++ plugins/orchestrator-common/api/models/schema.d.ts
@@ -5,25 +5,75 @@
 
 export interface paths {
   '/v2/workflows': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
     /** @description Get a list of workflow */
     get: operations['getWorkflows'];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
   };
   '/v2/workflows/{workflowId}': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
     /** @description Get a workflow by ID */
     get: operations['getWorkflowById'];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
   };
   '/v2/workflows/{workflowId}/overview': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
     /** @description Get a workflow overview by ID */
     get: operations['getWorkflowOverviewById'];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
   };
   '/v2/workflows/overview': {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
     /** @description Get a list of workflow overviews */
     get: operations['getWorkflowsOverview'];
+    put?: never;
+    post?: never;
+    delete?: never;
+    options?: never;
+    head?: never;
+    patch?: never;
+    trace?: never;
   };
 }
-
 export type webhooks = Record<string, never>;
-
 export interface components {
   schemas: {
     PaginationInfoDTO: {
@@ -75,23 +125,31 @@ export interface components {
   headers: never;
   pathItems: never;
 }
-
 export type $defs = Record<string, never>;
-
-export type external = Record<string, never>;
-
 export interface operations {
-  /** @description Get a list of workflow */
   getWorkflows: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
     responses: {
       /** @description Success */
       200: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': components['schemas']['WorkflowListResultDTO'];
         };
       };
       /** @description Error fetching workflow list */
       500: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': {
             /** @description Error message */
@@ -101,23 +159,32 @@ export interface operations {
       };
     };
   };
-  /** @description Get a workflow by ID */
   getWorkflowById: {
     parameters: {
+      query?: never;
+      header?: never;
       path: {
         /** @description Unique identifier of the workflow */
         workflowId: string;
       };
+      cookie?: never;
     };
+    requestBody?: never;
     responses: {
       /** @description Success */
       200: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': components['schemas']['WorkflowDTO'];
         };
       };
       /** @description Error fetching workflow */
       500: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': {
             /** @description Error message */
@@ -127,23 +194,32 @@ export interface operations {
       };
     };
   };
-  /** @description Get a workflow overview by ID */
   getWorkflowOverviewById: {
     parameters: {
+      query?: never;
+      header?: never;
       path: {
         /** @description Unique identifier of the workflow */
         workflowId: string;
       };
+      cookie?: never;
     };
+    requestBody?: never;
     responses: {
       /** @description Success */
       200: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': components['schemas']['WorkflowOverviewDTO'];
         };
       };
       /** @description Error fetching workflow overview */
       500: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': {
             /** @description Error message */
@@ -153,17 +229,29 @@ export interface operations {
       };
     };
   };
-  /** @description Get a list of workflow overviews */
   getWorkflowsOverview: {
+    parameters: {
+      query?: never;
+      header?: never;
+      path?: never;
+      cookie?: never;
+    };
+    requestBody?: never;
     responses: {
       /** @description Success */
       200: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': components['schemas']['WorkflowOverviewListResultDTO'];
         };
       };
       /** @description Error fetching workflow overviews */
       500: {
+        headers: {
+          [name: string]: unknown;
+        };
         content: {
           'application/json': {
             /** @description Error message */
