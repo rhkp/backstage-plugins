f543f03a13cedee1c589766bb38c0de9d96d47cd
Kamlesh Panchal <kpanchal@redhat.com>
2024-01-29 11:16:14 +0100

    Implement executeWorkflowById (V2)
    Signed-off-by: Kamlesh Panchal <kpanchal@redhat.com>
    
    -----------------------------------------------------
    
    Re-arrange commits
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- plugins/orchestrator-backend/src/service/router.ts
+++ plugins/orchestrator-backend/src/service/router.ts
@@ -29,6 +29,8 @@ import { CloudEventService } from './CloudEventService';
 import { DataIndexService } from './DataIndexService';
 import { DataInputSchemaService } from './DataInputSchemaService';
 import {
+  executeWorkflowByIdV1,
+  executeWorkflowByIdV2,
   getInstancesByIdV1,
   getInstancesByIdV2,
   getInstancesV1,
@@ -275,24 +277,36 @@ function setupInternalRoutes(
       params: { workflowId },
     } = req;
 
-    const definition = await dataIndexService.getWorkflowDefinition(workflowId);
-    const serviceUrl = definition.serviceUrl;
-    if (!serviceUrl) {
-      throw new Error(`ServiceURL is not defined for workflow ${workflowId}`);
-    }
-    const executionResponse = await sonataFlowService.executeWorkflow({
+    await executeWorkflowByIdV1(
+      dataIndexService,
+      sonataFlowService,
+      req.body,
       workflowId,
-      inputData: req.body,
-      endpoint: serviceUrl,
-    });
-
-    if (!executionResponse) {
-      res.status(500).send(`Couldn't execute workflow ${workflowId}`);
-      return;
-    }
-
-    res.status(200).json(executionResponse);
+    )
+      .then((result: any) => res.status(200).json(result))
+      .catch((error: { message: any }) => {
+        res.status(500).send(error.message || 'Internal Server Error');
+      });
   });
+
+  // v2
+  api.register(
+    'executeWorkflow',
+    async (c, req: express.Request, res: express.Response, next) => {
+      const workflowId = c.request.params.workflowId as string;
+      const executeWorkflowRequestDTO = req.body;
+      await executeWorkflowByIdV2(
+        dataIndexService,
+        sonataFlowService,
+        executeWorkflowRequestDTO,
+        workflowId,
+      )
+        .then(result => res.status(200).json(result))
+        .catch((error: { message }) => {
+          res.status(500).send(error.message || 'Internal Server Error');
+        });
+    },
+  );
 
   router.get('/workflows/:workflowId/overview', async (req, res) => {
     const {
